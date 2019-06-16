765b9358fed76dc9ed31e66d7ae24206f60bbb92
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2019-06-16 22:57:08 +0200

    Implemented a hyperlink-cleaning mechanism for sentence processor

--- randsentlist/sentence/processor.py
+++ randsentlist/sentence/processor.py
@@ -1,3 +1,6 @@
+import re
+
+
 class SentenceProcessor:
     def __init__(self):
         self._minimum_length = 30
@@ -5,6 +8,11 @@ class SentenceProcessor:
     def _long_enough(self, sentence):
         return len(sentence) >= self._minimum_length
 
+    def _clean_hyperlinks(self, sentence):
+        return re.sub("\[(.*?)\]", "", sentence)
+
     def to_sentences(self, text):
-        sentences = (sentence for sentence in text.split(".") if self._long_enough(sentence))
-        return sorted(sentences, key=len, reverse=True)
+        sentences = [sentence for sentence in text.split(".") if self._long_enough(sentence)]
+        sentences = list(map(self._clean_hyperlinks, sentences))
+        sentences.sort(key=len, reverse=True)
+        return sentences
