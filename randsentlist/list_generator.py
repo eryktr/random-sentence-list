840e026d110157f10ac6faa9d259d843c2779917
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2019-06-17 23:57:44 +0200

    Implemented list generator

--- randsentlist/list_generator.py
+++ randsentlist/list_generator.py
@@ -1,11 +1,25 @@
+from randsentlist.filename.filename import unique_filename
 from randsentlist.latex.latex_writer import LatexWriter
 from randsentlist.request.page_fetcher import PageFetcher
 from randsentlist.sentence.processor import SentenceProcessor
 
 
 class ListGenerator:
-    def __init__(self, num_sentences):
-        self._num_sentences = num_sentences
+    def __init__(self):
         self._writer = LatexWriter()
         self._fetcher = PageFetcher()
         self._processor = SentenceProcessor()
+
+    def set_language(self, language):
+        self._fetcher.set_language(language)
+
+    def random_sentences_file(self, sentences_per_file, num_files):
+        for _ in range(num_files):
+            sentences = set()
+            name = unique_filename()
+            while len(sentences) < sentences_per_file:
+                pages = self._fetcher.random_wiki_pages(sentences_per_file)
+                longest_sentences = [self._processor.to_sentences(page.content)[0] for page in pages]
+                for sentence in longest_sentences:
+                    sentences.add(sentence)
+            self._writer.write_iterable_to_latex_file(sentences, name)
