c5b7b59be152796792e5f65426aba28be42e4400
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2019-06-22 22:31:45 +0200

    Added one more requirement for the sentence not to be a quotation

--- randsentlist/sentence/validator.py
+++ randsentlist/sentence/validator.py
@@ -10,6 +10,10 @@ class Validator:
         return not any(c in sentence for c in ["\\", "=", "+", "-", "*", "/", ":"])
 
     @staticmethod
+    def _has_no_quotes(sentence: str):
+        return "\"" not in sentence
+
+    @staticmethod
     def _is_single_line(sentence: str):
         return "\n" not in sentence
 
@@ -17,4 +21,5 @@ class Validator:
     def is_valid_sentence(sentence):
         return Validator._is_long_enough(sentence) \
                and Validator._is_not_equation(sentence) \
-               and Validator._is_single_line(sentence)
+               and Validator._is_single_line(sentence) \
+               and Validator._has_no_quotes(sentence)
